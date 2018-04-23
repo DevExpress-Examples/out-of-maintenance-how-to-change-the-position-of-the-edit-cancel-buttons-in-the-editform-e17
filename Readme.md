4f2faf687fa682574bf9507e2034bc2d5dd450e5
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to change the position of the Edit/Cancel buttons in the EditForm
+
+
+<p>Use the EditFormTemplate to adjust the buttons' position as you require</p>
+
+<br/>
+
+
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,21 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Data
+Imports System.Configuration
+Imports System.Web
+Imports System.Web.Security
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports System.Web.UI.WebControls.WebParts
+Imports System.Web.UI.HtmlControls
+Imports DevExpress.Web
+Imports DevExpress.Xpo
+Imports System.Drawing
+Imports System.Collections.Generic
+Imports DevExpress.Data
+Imports System.Collections
+
+Partial Public Class Change_Update_Cancel_Button_Position
+	Inherits System.Web.UI.Page
+
+End Class
