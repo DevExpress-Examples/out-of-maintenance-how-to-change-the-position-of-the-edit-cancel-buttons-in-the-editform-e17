5346ce79cab42e28b638c559145ca2a04b644294
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,24 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Data
+Imports System.Configuration
+Imports System.Web
+Imports System.Web.Security
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports System.Web.UI.WebControls.WebParts
+Imports System.Web.UI.HtmlControls
+Imports DevExpress.Web.ASPxTabControl
+Imports DevExpress.Xpo
+Imports System.Drawing
+Imports DevExpress.Web.ASPxGridView
+Imports DevExpress.Web.ASPxEditors
+Imports System.Collections.Generic
+Imports DevExpress.Data
+Imports DevExpress.Web.ASPxDataView
+Imports System.Collections
+
+Partial Public Class Change_Update_Cancel_Button_Position
+	Inherits System.Web.UI.Page
+
+End Class
